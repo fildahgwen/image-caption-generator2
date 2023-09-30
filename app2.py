ede2f79f87eba7b656835fe643e671bda325787a
Tariro Gwandiwa <95386578+fildahgwen@users.noreply.github.com>
2023-09-30 05:16:38 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -88,7 +88,8 @@ def main():
     if choice == "Upload":
         st.subheader("Upload Your Video")
 
-        video_file_path = st.file_uploader("accepting mp4,avi", type=["mp4", "avi"],server.maxUploadSize = 2)
+        video_file_path = st.file_uploader("accepting mp4,avi", type=["mp4", "avi"])
+        st.set_option('server.maxUploadSize', 2 * 1024 * 1024)  # Set maximum upload size to 2 megabytes
 
         if video_file_path is not None:
             path = video_file_path.name
