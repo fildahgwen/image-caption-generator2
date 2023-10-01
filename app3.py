f331d905142d160dce68763f93d083d2afd5def9
Tariro Gwandiwa <95386578+fildahgwen@users.noreply.github.com>
2023-10-01 18:18:38 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -10,75 +10,95 @@ import cv2
 import numpy as np
 from keras.models import load_model
 import sys
-from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menupython
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from tokenizer import Tokenizer
+python
+from PIL import Image
 
 
-# Load MobileNetV2 model
-mobilenet_model = MobileNetV2(weights="imagenet")
-mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.layers[-2].output)
 
-# Load your trained model
-model = tf.keras.models.load_model('mymodel.h5')
+# Load the model and tokenizer
+model = load_model('model.h5')
+tokenizer = Tokenizer.from_json('tokenizer.json')
 
-# Load the tokenizer
-with open('tokenizer.pkl', 'rb') as tokenizer_file:
-    tokenizer = pickle.load(tokenizer_file)
-    
-# Set custom web page title
-st.set_page_config(page_title="Caption Generator App")
-
-# Streamlit app
-st.title("Image Caption Generator")
-st.markdown(
-    "Upload a video, and this app will generate a caption for it using a trained model."
-)
+# Load the MobileNet model for feature extraction
+mobile_net_model = load_model('mobilenet_model.h5')
 
 
-# Load the pre-trained model
-model = MobileNetV2(weights='imagenet', include_top=True)
+# Load the model and tokenizer
+model = load_model('model.h5')
+tokenizer = Tokenizer.from_json('tokenizer.json')
 
-# Function to extract features from the image
-def extract_features(image):
+# Load the MobileNet model for feature extraction
+mobile_net_model = load_model('mobilenet_model.h5')
+
+# Function to generate captions from image
+def generate_caption(image):
+    # Preprocess the image
     image = cv2.resize(image, (224, 224))
+    image = image.astype('float32')
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
-    image = preprocess_input(image)
-    features = model.predict(image)
-    return features
 
-# Function to perform image captioning
-def image_captioning(image):
-     # Generate caption using the model
-        def predict_caption(model, image_features, tokenizer, max_caption_length):
-            caption = "startseq"
-            for _ in range(max_caption_length):
-                sequence = tokenizer.texts_to_sequences([caption])[0]
-                sequence = pad_sequences([sequence], maxlen=max_caption_length)
-                yhat = model.predict([image_features, sequence], verbose=0)
-                predicted_index = np.argmax(yhat)
-                predicted_word = get_word_from_index(predicted_index, tokenizer)
-                caption += " " + predicted_word
-                if predicted_word is None or predicted_word == "endseq":
-                    break
-            return caption
+    # Extract features from the image using MobileNet
+    features = mobile_net_model.predict(image)
 
-        # Generate caption
-        generated_caption = predict_caption(model, image_features, tokenizer, max_caption_length)
+    # Generate a caption using the model and tokenizer
+    caption = ''
+    for i in range(max_caption_length):
+        sequence = tokenizer.sequences_to_texts([caption_sequence])[0]
+        word_index = tokenizer.word_index[sequence.split()[-1]] + 1
+        next_word_index = np.argmax(word_probabilities[i][word_index])
+        next_word = tokenizer.index_word[next_word_index]
+        if next_word == '<end>':
+            break
+        else:
+            caption += ' ' + next_word
+    return caption
 
-        # Remove startseq and endseq
-        generated_caption = generated_caption.replace("startseq", "").replace("endseq", "")
-        pass
+# Function to process video frames
+def process_video_frames(video_frames):
+    captions = []
+    for frame in video_frames:
+        caption = generate_caption(frame)
+        captions.append(caption)
+    return captions
+
+# Function to display video with captions
+def display_video_with_captions(video_path, captions):
+    video = cv2.VideoCapture(video_path)
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+        caption = captions.pop(0)
+        cv2.putText(frame, caption, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow('Video with Captions', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video.release()
+    cv2.destroyAllWindows()
 
 # Streamlit app
-st.title('Image Captioning on Video Clips')
-uploaded_file = st.file_uploader('Choose a video file', type=['mp4', 'avi', 'mov', 'mkv'])
-
+st.title('Video Captioning App')
+uploaded_file = st.file_uploader('Choose a video file', type=['mp4', 'avi'])
 if uploaded_file is not None:
-    video = cv2.VideoCapture(uploaded_file)
-    success, image = video.read()
-    while success:
-        features = extract_features(image)
-        caption = image_captioning(features)
-        st.write(f'Caption: {caption}')
-        success, image = video.read()
+    video_path = 'temp.mp4'
+    with open(video_path, 'wb') as f:
+        f.write(uploaded_file.getbuffer())
+    video = cv2.VideoCapture(video_path)
+    video_frames = []
+    while video.isOpened():
+        ret, frame = video.read()
+        if not ret:
+            break
+        video_frames.append(frame)
     video.release()
+    captions = process_video_frames(video_frames)
+    display_video_with_captions(video_path, captions)
