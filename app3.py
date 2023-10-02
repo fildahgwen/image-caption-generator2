ea7bc7f58a9d2da9bc5623fa835c12f564bf5a7c
Tariro Gwandiwa <95386578+fildahgwen@users.noreply.github.com>
2023-10-02 14:30:04 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -21,103 +21,92 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from PIL import Image
 
 
+# Load the pre-trained ResNet-50 model
+resnet_model = ResNet50(weights="imagenet", include_top=False, pooling="avg")
 
+# Define the prediction function
+def predict_caption(photo):
+    in_text = "startseq"
+    max_len = 29
 
-# Load MobileNetV2 model
-mobilenet_model = MobileNetV2(weights="imagenet")
-mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.layers[-2].output)
+    # Load word_to_idx dictionary from file
+    with open("saved_ixtoword.pkl", "rb") as f:
+        word_to_idx = pickle.load(f)
 
-# Load your trained model
-model = tf.keras.models.load_model('mymodel.h5')
+    # Load idx_to_word dictionary from file
+    with open("saved_ixtoword.pkl", "rb") as f:
+        idx_to_word = pickle.load(f)
 
-# Load the tokenizer
-with open('tokenizer.pkl', 'rb') as tokenizer_file:
-    tokenizer = pickle.load(tokenizer_file)
-    
-# Set custom web page title
-st.set_page_config(page_title="Caption Generator App")
+    # Placeholder for the image captioning model
+    #model = load_model("final_mod.h5")
+
+    for _ in range(max_len):
+        sequence = [word_to_idx[w] for w in in_text.split() if w in word_to_idx]
+        sequence = pad_sequences([sequence], maxlen=max_len, padding='post')
+
+        ypred = model.predict([photo, sequence])
+        ypred = ypred.argmax()
+        if ypred not in idx_to_word:
+            break
+        word = idx_to_word[ypred]
+        in_text += ' ' + word
+
+        if word == 'endseq':
+            break
+
+    final_caption = in_text.split()
+    final_caption = final_caption[1:-1]
+    final_caption = ' '.join(final_caption)
+
+    return final_caption
 
 # Streamlit app
-st.title("Image Caption Generator")
-st.markdown(
-    "Upload an image, and this app will generate a caption for it "
-)
+def main():
+    st.title("Video Description Generator")
 
-# Upload image
-uploaded_image = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+ 
 
-# Process uploaded image
-if uploaded_image is not None:
-    st.subheader("Uploaded Image")
-    st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
-
-    st.subheader("Generated Caption")
-    # Display loading spinner while processing
-    with st.spinner("Generating caption..."):
-        # Load image
-        image = load_img(uploaded_image, target_size=(224, 224))
-        image = img_to_array(image)
-        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-        image = preprocess_input(image)
-
-        # Extract features using VGG16
-        image_features = mobilenet_model.predict(image, verbose=0)
-
-        # Max caption length
-        max_caption_length = 34
+   
         
-        # Define function to get word from index
-        def get_word_from_index(index, tokenizer):
-            return next(
-                (word for word, idx in tokenizer.word_index.items() if idx == index), None
-        )
 
-                 # Function to process video frames
-def process_video_frames(video_frames):
-    captions = []
-    for frame in video_frames:
-        caption = generate_caption(frame)
-        captions.append(caption)
-    return captions
+    # Upload video file
+    video_file = st.file_uploader("Upload Video ", type=["mp4"])
+    
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(video_file.read())
+            video_path = temp_file.name
 
-# Function to display video with captions
-def display_video_with_captions(video_path, captions):
-    video = cv2.VideoCapture(video_path)
-    while video.isOpened():
-        ret, frame = video.read()
-        if not ret:
-            break
-        caption = captions.pop(0)
-        cv2.putText(frame, caption, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.imshow('Video with Captions', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    video.release()
-    cv2.destroyAllWindows()
- # Generate caption using the model
-def predict_caption(model, image_features, tokenizer, max_caption_length):
-        caption = "startseq"
-        for _ in range(max_caption_length):
-            sequence = tokenizer.texts_to_sequences([caption])[0]
-            sequence = pad_sequences([sequence], maxlen=max_caption_length)
-            yhat = model.predict([image_features, sequence], verbose=0)
-            predicted_index = np.argmax(yhat)
-            predicted_word = get_word_from_index(predicted_index, tokenizer)
-            caption += " " + predicted_word
-            if predicted_word is None or predicted_word == "endseq":
-                break
-        return caption       
+        # Convert video frames to images
 
-        # Generate caption
-generated_caption = predict_caption(model, image_features, tokenizer, max_caption_length)
 
-        # Remove startseq and endseq
-generated_caption = generated_caption.replace("startseq", "").replace("endseq", "")
+      
+    frames = []
+    vidcap = cv2.VideoCapture(video_path)
+    success, image = vidcap.read()
+    while success:
+        frames.append(image)
+        success, image = vidcap.read()
 
-    # Display the generated caption with custom styling
-st.markdown(
-    f'<div style="border-left: 6px solid #ccc; padding: 5px 20px; margin-top: 20px;">'
-    f'<p style="font-style: italic;">“{generated_caption}”</p>'
-    f'</div>',
-    unsafe_allow_html=True
-)
+        # Process each frame and predict captions
+     
+    for i, frame in enumerate(frames):
+            # Resize the frame to the input size of the ResNet-50 model
+        frame = cv2.resize(frame, (224, 224))
+
+            # Preprocess the image
+        img = preprocess_input(frame)
+
+            # Pass the image through the ResNet-50 model
+        img_features = resnet_model.predict(np.expand_dims(img, axis=0))
+
+            # Get the predicted caption
+        caption = predict_caption(img_features)
+
+            # Display the frame and the predicted caption
+    st.image(frame, use_column_width=True)
+    st.write(f" Frame {i+1}: {caption}")
+
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()     
+
