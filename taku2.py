78331717587bf20100282fca7a6da7f9c0f2e5c1
Tariro Gwandiwa <95386578+fildahgwen@users.noreply.github.com>
2023-10-02 11:49:42 +0200

    Add files via upload

---
+++ taku2.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+import tempfile
+import pickle
+import streamlit as st
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import cv2
+import tensorflow as tf 
+import numpy as np
+from keras.models import load_model
+import sys
+from streamlit_option_menu import option_menu
+
+# Load MobileNetV2 model
+mobilenet_model = MobileNetV2(weights="imagenet")
+mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.layers[-2].output)
+
+# Load your trained model
+model = tf.keras.models.load_model('mymodel.h5')
+
+# Load the tokenizer
+with open('tokenizer.pkl', 'rb') as tokenizer_file:
+    tokenizer = pickle.load(tokenizer_file)
+    
+# Set custom web page title
+st.set_page_config(page_title="Caption Generator App")
+
+# Streamlit app
+st.title("Image Caption Generator")
+st.markdown(
+    "Upload an image, and this app will generate a caption for it "
+)
+
+# Upload image
+# Upload video file
+video_file = st.file_uploader("Upload Video ", type=["mp4"])
+    
+with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(video_file.read())
+        video_path = temp_file.name
+
+        # Convert video frames to images
+
+
+      
+frames = []
+vidcap = cv2.VideoCapture(video_path)
+success, image = vidcap.read()
+while success:
+    frames.append(image)
+    success, image = vidcap.read()
+
+        # Process each frame and predict captions
+     
+for i, frame in enumerate(frames):
+            # Resize the frame to the input size of the ResNet-50 model
+    frame = cv2.resize(frame, (224, 224))
+
+            # Preprocess the image
+     uploaded_image= preprocess_input(frame)
+
+#uploaded_image = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+# Process uploaded image
+if uploaded_image is not None:
+    st.subheader("Uploaded Image")
+    st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+    st.subheader("Generated Caption")
+    # Display loading spinner while processing
+    with st.spinner("Generating caption..."):
+        # Load image
+        image = load_img(uploaded_image, target_size=(224, 224))
+        image = img_to_array(image)
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        image = preprocess_input(image)
+
+        # Extract features using VGG16
+        image_features = mobilenet_model.predict(image, verbose=0)
+
+        # Max caption length
+        max_caption_length = 34
+        
+        # Define function to get word from index
+        def get_word_from_index(index, tokenizer):
+            return next(
+                (word for word, idx in tokenizer.word_index.items() if idx == index), None
+        )
+
+        # Generate caption using the model
+        def predict_caption(model, image_features, tokenizer, max_caption_length):
+            caption = "startseq"
+            for _ in range(max_caption_length):
+                sequence = tokenizer.texts_to_sequences([caption])[0]
+                sequence = pad_sequences([sequence], maxlen=max_caption_length)
+                yhat = model.predict([image_features, sequence], verbose=0)
+                predicted_index = np.argmax(yhat)
+                predicted_word = get_word_from_index(predicted_index, tokenizer)
+                caption += " " + predicted_word
+                if predicted_word is None or predicted_word == "endseq":
+                    break
+            return caption
+
+        # Generate caption
+        generated_caption = predict_caption(model, image_features, tokenizer, max_caption_length)
+
+        # Remove startseq and endseq
+        generated_caption = generated_caption.replace("startseq", "").replace("endseq", "")
+
+    # Display the generated caption with custom styling
+    st.markdown(
+        f'<div style="border-left: 6px solid #ccc; padding: 5px 20px; margin-top: 20px;">'
+        f'<p style="font-style: italic;">“{generated_caption}”</p>'
+        f'</div>',
+        unsafe_allow_html=True
+    )
